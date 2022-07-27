9b9fa87917755fcbc0256b1417ffdd1e373cf4d1
miroslavuzunow <miroslavuzunow@gmail.com>
2022-07-27 11:30:11 +0300

    Add another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Ramge Egg')
 streamlit.text('🥑🍞 Avocado Toas')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
