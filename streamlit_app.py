776f4bfbd4dd007701c297afd11f07545a14b50c
miroslavuzunow <miroslavuzunow@gmail.com>
2022-07-29 15:05:18 +0300

    Format the response as table

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# Transforming the json to panda dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# show table formated JSON
+streamlit.dataframe(fruityvice_normalized)
