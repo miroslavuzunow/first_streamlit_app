a5f2d5070aa1a528db4acdfb00e5ebecf426741a
miroslavuzunow <miroslavuzunow@gmail.com>
2022-07-29 16:19:46 +0300

    Add second box

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,6 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+fruit_choice_2 = streamlit.text_input('What fruit would you like to add?'my_data_rows)
+streamlit.write('The user entered ', fruit_choice_2)
